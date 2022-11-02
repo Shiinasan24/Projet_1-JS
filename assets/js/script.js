772a3c33d2daa804d829535d503b9947e6c58348
Anthony Bonis <bonis.anthony@gmail.com>
2022-11-02 10:57:45 +0100

    Ajout d'un age prompt à l'entrée du site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,39 @@
 
+// ------------- VARIABLES ------------- //
 
-// VARIABLES //
+// Menu Burger ---
 
-
-let menuBurger = document.querySelector(".burger");
-let navMobile = document.querySelector(".navMobile");
-
+const menuBurger = document.querySelector(".burger");
+const navMobile = document.querySelector(".navMobile");
 menuBurger.addEventListener("click", displayNav);
 
+// Age Prompt --- 
 
-// FUNCTIONS //
+const bodyDisplay = document.querySelector("body")
+const bodyHide = document.querySelector(".hide")
 
+// ------------- FUNCTIONS ------------- //
 
-// navMobile DISPLAY ON BURGER CLICK --
+// navMobile DISPLAY ON BURGER CLICK ---
 function displayNav() {
     navMobile.classList.toggle("toggleDisplay");
 }
 
-console.log(hideOnScroll);
+// agePrompt when entering website ---
+function agePrompt() {
+
+    const age = prompt("Quel est votre age ?");
+
+    if (age < 18) {
+        alert(age + " ans ? Y'a rien à voir ici pour toi, kiddo.");
+    } else {
+        alert(age + " ans ? Enjoy.");
+        bodyDisplay.classList.remove("hide")
+    }
+}
+
+
+// ------------- FUNCTION CALLS ------------- //
+
+// Asking for age when entering website ---
+agePrompt()
