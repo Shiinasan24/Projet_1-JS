17869fa6ee9324c462aea1e92c5db27f0aed6f6b
Anthony Bonis <bonis.anthony@gmail.com>
2022-11-08 15:22:12 +0100

    Ajout d'un mini jeu avec un Math.random() et une petite animation sur victoire

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,33 @@ const isUsLegal = " years old ? You're good to go.";
 const isUsNotLegal = " years old ? Turn around, you're too young.";
 
 
+// var - Error pop-up :
+
+const legalResponse = document.querySelector(".legalResponse")
+
+
+// MINI JEU
+
+const btnStartMiniJeu = document.getElementById("btnStartMiniJeu")
+const displayNumberToBeFound = document.getElementById("numberToBeFound")
+
+btnStartMiniJeu.addEventListener("click", startMiniJeu)
+
+// Chara 1
+const chara1 = document.getElementById("chara1")
+let drawChara1
+const numberDrawnRed = document.getElementById("numberDrawnRed")
+const displayScoreRed = document.getElementById("scoreRed")
+let scoreRed = 0
+
+// Chara 2
+const chara2 = document.getElementById("chara2")
+let drawChara2
+const numberDrawnBlue = document.getElementById("numberDrawnBlue")
+const displayScoreBlue = document.getElementById("scoreBlue")
+let scoreBlue = 0
+
+
 // ----------------------- FUNCTIONS ----------------------- //
 
 // Fn - navMobile DISPLAY ON BURGER CLICK :
@@ -42,10 +69,9 @@ function displayNav() {
 
 function agePrompt() {  
 
-
     if (country.value == "FR") {
         if (age.value < legalAgeFr) {
-            alert(age.value + isFrNotLegal);
+            alert(age.value + isFrNotLegal)
         } else {
             alert(age.value + isFrLegal)
         } 
@@ -65,7 +91,145 @@ function agePrompt() {
 }   
 
 
-// ----------------------- FUNCTION CALLS ----------------------- //
+// MINI JEU
 
-// Fc - Asking for age when entering website :
-// agePrompt("JP")
+function startMiniJeu() {
+
+    toBeFound = drawNumbers(10)
+    drawChara1 = drawNumbers(10)
+    drawChara2 = drawNumbers(10)
+    
+    displayNumberToBeFound.innerHTML = toBeFound
+    numberDrawnRed.innerHTML = "Nombre tiré : " + drawChara1
+    numberDrawnBlue.innerHTML = "Nombre tiré : " + drawChara2
+    
+    if (drawChara1 === toBeFound) {
+        scoreRed++
+        displayScoreRed.innerHTML = scoreRed
+        chara = 0;
+        charaWin()
+        
+    } else if (drawChara2 === toBeFound) {
+        scoreBlue++
+        displayScoreBlue.innerHTML = scoreBlue
+        chara = 1;
+        charaWin()
+    }
+}
+
+// fn - Draw random number
+function drawNumbers(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function charaWin() {
+    if (chara === 0) {
+        animationRed()
+    } else if (chara === 1) {
+        animationBlue()
+    }
+}
+
+// ----------------------------------------
+function animationRed() {
+
+    chara1.innerHTML = chara1.classList.toggle("chara1BaseRed")
+    chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+    
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+    }, "500");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+    }, "1000");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+    }, "1500");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+    }, "2000");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+    }, "2500");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+    }, "3000");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+    }, "3500");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win1Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+    }, "4000");
+
+    setTimeout(() => {
+        chara1.innerHTML= chara1.classList.toggle("chara1Win0Red")
+        chara1.innerHTML= chara1.classList.toggle("chara1BaseRed")
+    }, "4200");
+}
+
+// ----------------------------------------
+function animationBlue() {
+
+    chara2.innerHTML = chara2.classList.toggle("chara2BaseBlue")
+    chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+    
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+    }, "500");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+    }, "1000");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+    }, "1500");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+    }, "2000");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+    }, "2500");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+    }, "3000");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+    }, "3500");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win1Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+    }, "4000");
+
+    setTimeout(() => {
+        chara2.innerHTML= chara2.classList.toggle("chara2Win0Blue")
+        chara2.innerHTML= chara2.classList.toggle("chara2BaseBlue")
+    }, "4200");
+}
